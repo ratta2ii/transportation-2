97b98afddeb384f1b226222fa85b8e056239ba37
Trevor Mackin and Will Quanstrom <trevor@>
2019-10-29 14:26:57 -0700

    Add checkboxes. Capturecheckbox values using jQuery. Append text into hidden paragraphs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
+  
+  $("form.form-1").submit(function(event){
+    event.preventDefault();
 
+    $("#hidden-text").show();
+    $("input:checkbox[name=work-transportation]:checked").each(function(){
+      var workCheckedValues = $(this).val();
+      $("#hidden-text").append(workCheckedValues + "<br>");
+    });
 
+    $("#hidden-text-2").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      var funCheckedValues = $(this).val();
+      $("#hidden-text-2").append(funCheckedValues + "<br>");
+    });
 
+    $(".form-1").hide();
+
+  });
 });
